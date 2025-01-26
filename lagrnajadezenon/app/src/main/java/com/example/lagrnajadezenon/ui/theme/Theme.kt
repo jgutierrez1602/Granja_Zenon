81b8160b62e34b1c1d3ba3cc7e688c63291ee7e0
jgutierrez1602 <42759043+jgutierrez1602@users.noreply.github.com>
2025-01-25 23:14:27 -0500

    cc

---
+++ lagrnajadezenon/app/src/main/java/com/example/lagrnajadezenon/ui/theme/Theme.kt
@@ -0,0 +1,31 @@
+package com.example.lagrnajadezenon.ui.theme
+
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.material3.*
+
+@Composable
+fun MyAppTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colorScheme = if (darkTheme) {
+        darkColorScheme()
+    } else {
+        lightColorScheme()
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
