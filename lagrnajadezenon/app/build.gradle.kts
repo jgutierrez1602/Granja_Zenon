cfcd30efc75426173c0db9c2759dfcc0c428a01c
jgutierrez1602 <42759043+jgutierrez1602@users.noreply.github.com>
2025-01-28 22:42:34 -0500

    CC

--- lagrnajadezenon/app/build.gradle.kts
+++ lagrnajadezenon/app/build.gradle.kts
@@ -36,4 +36,9 @@ dependencies {
     implementation("androidx.compose.material3:material3:1.0.1")
     implementation("androidx.compose.ui:ui-tooling-preview:1.4.3")
     debugImplementation("androidx.compose.ui:ui-tooling:1.4.3")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
 }
